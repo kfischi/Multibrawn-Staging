c3fbe29b8a7d9267cb8e8fde78af9c8deff38788
kfischi <kfir.biz@gmail.com>
2026-01-24 17:11:03 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,19 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**', // מאפשר את כל התמונות מכל תיקייה ומכל ענן בקלודינרי
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.tzimer360.co.il',
+        hostname: 'MultibrawnWEB.b-cdn.net', // הכתובת החדשה שלך
         pathname: '/**',
       },
+      // ... שאר הכתובות
     ],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
-  compress: true,
 };
-
 export default nextConfig;
