49be3f02f894ef0df384c63849fb9d5b4b68c32e
kfischi <kfir.biz@gmail.com>
2026-01-24 14:01:19 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,20 +5,18 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        //pathname: '/**'  // מאפשר את כל הנתיבים בקלודינרי עבור שני החשבונות שלך
+        pathname: '/**', // מאפשר את כל התמונות מכל תיקייה ומכל ענן בקלודינרי
       },
       {
         protocol: 'https',
         hostname: 'www.tzimer360.co.il',
-      }
+        pathname: '/**',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   compress: true,
-  // אם הקובץ הוא next.config.mjs השתמש בייצוא הזה:
-  // export default nextConfig;
 };
 
-// אם הקובץ הוא next.config.js השתמש בזה:
-module.exports = nextConfig;
+export default nextConfig;
